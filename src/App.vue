9cdcff312218c5f75502659a53eddcc2cfc5dae1
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-05-20 11:59:09 +0200

    inserimento componenti

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <HeaderVue/>
+    <MainVue />
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainVue from './components/Main.vue'
+import HeaderVue from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderVue,
+    MainVue
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="head">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: HeaderVue
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
