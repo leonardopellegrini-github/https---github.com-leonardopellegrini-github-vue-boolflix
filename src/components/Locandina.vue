59af2fc82211748815993d227e766faa7759612d
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-05-20 15:49:48 +0200

    problema con le API

--- src/components/Locandina.vue
+++ src/components/Locandina.vue
@@ -1,22 +1,31 @@
 <template>
-  <div id="locand">
-    
+  
+
+  <div>
+    <div class="locand">
+      
+    </div>
+
+
   </div>
+  
 </template>
 
 <script>
 export default {
-  name: 'LocandinaVue'
-
+  name: 'LocandinaVue',
+ 
 }
 </script>
 
 <style lang="scss" scoped>
 
-  #locand{
+  .locand{
     height: 250px;
     width: 180px;
     background-color: red;
-  }
+    margin: 5px;
+    float: left;
+}
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,13 +9,38 @@
 </template>
 
 <script>
+import axios from 'axios'
 import LocandinaVue from './Locandina.vue'
 
 export default {
   name: 'MainVue',
   components: {
     LocandinaVue
-}
+  },
+  
+  data(){
+    return{
+      apiUrl: 'https://api.themoviedb.org/3/movie/550?api_key=f16b4e0d1ad76ea4f01b26e4ad97562b&language=it-',
+    }
+  },
+  
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+      .then(r =>{
+        console.log(r.data)
+        this.elencofilm = r.data.response;
+      })
+      .catch(e =>{
+        console.log(e)
+      })
+    },
+  },
+
+  mounted(){
+    this.getApi();
+  },
+
 }
 </script>
 
